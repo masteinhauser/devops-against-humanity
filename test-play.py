4ee6ee52333dcc47c7ce02ff946bf597d7fc7e9d
Myles Steinhauser <myles.steinhauser@gmail.com>
2015-12-14 17:48:07 -0500

    test-play read from local file, not url

--- test-play.py
+++ test-play.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import urllib,csv,random,re
-u='https://raw.githubusercontent.com/masteinhauser/devops-against-humanity/master/cards-DevOpsAgainstHumanity.csv'
 
 white=[]
 black=[]
-for r in csv.reader(urllib.urlopen(u)):
-    if r[0]: white.append(r[0])
-    if r[1]: black.append(r[1])
+
+with open('cards-DevOpsAgainstHumanity.csv', 'rb') as csvfile:
+    for r in csv.reader(csvfile):
+        if r[0]: white.append(r[0])
+        if r[1]: black.append(r[1])
 
 blank=re.compile(r"( +)?(\b|[^_])_+(\b|[^_])( +)?")
 rpl=lambda _:" %s " % random.choice(white)
